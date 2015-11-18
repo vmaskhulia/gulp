64c6da5a9ec4918b15d748715ad894c0ec387f69
Vato Maskhulia <exprominent@gmail.com>
2015-11-19 01:18:36 +0400

    update

--- consts.js
+++ consts.js
@@ -6,6 +6,7 @@ export const NAME = pkg.name;
 export const SSH_ADDRESS = pkg.gulp.sshAddress;
 export const LOCALHOST_PORT = pkg.gulp.developmentPort;
 export const BROWSER_SYNC_PORT = pkg.gulp.browserSyncPort;
+export const LUCY_SSH_ADDRESS = 'lucy@46.101.194.91';
 
 export const FILES_TO_INJECT_FOR_DIST = [
   pkg.gulp.externalHelpersPath,
--- tasks/release.js
+++ tasks/release.js
@@ -1,18 +1,24 @@
 'use strict';
 
 import gulp from 'gulp';
-import {NAME, SSH_ADDRESS} from '../consts';
+import {NAME, SSH_ADDRESS, LUCY_SSH_ADDRESS} from '../consts';
 import path from 'path';
+var $ = require('gulp-load-plugins')();
 
 var rootPath = path.normalize(__dirname + '/../..');
+
+const argv = $.util.env;
 
 gulp.task('release', ['dist'], () => {
   var exec = require('child_process').exec;
 
+  var lucyFlag = argv.lucy || argv.l;
+  var address = lucyFlag ? LUCY_SSH_ADDRESS : SSH_ADDRESS;
+
   var proc = exec(`
-      ssh ${SSH_ADDRESS} "mkdir -p ${NAME} && rm -r -f ${NAME}/dist ${NAME}/server"
-      scp -r ${rootPath}/dist ${rootPath}/server ${rootPath}/package.json ${SSH_ADDRESS}:~/${NAME}
-      ssh ${SSH_ADDRESS} "nvm use 4 && cd ${NAME} && npm i --production --no-optional"
+      ssh ${address} "mkdir -p ${NAME} && rm -r -f ${NAME}/dist ${NAME}/server"
+      scp -r ${rootPath}/dist ${rootPath}/server ${rootPath}/package.json ${address}:~/${NAME}
+      ssh ${address} "nvm use 4 && cd ${NAME} && npm i --production --no-optional"
     `);
 
   proc.stdout.on('data', data => console.log(data));
