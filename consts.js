7bd507e4e812f867eb1ee3d28a4dd9e35aea2ee3
Vato Maskhulia <exprominent@gmail.com>
2015-11-19 01:01:08 +0400

    update

--- consts.js
+++ consts.js
@@ -4,7 +4,7 @@ import pkg from '../package.json';
 
 export const NAME = pkg.name;
 export const SSH_ADDRESS = pkg.gulp.sshAddress;
-export const LOCALHOST_PORT = pkg.gulp.localhostPort;
+export const LOCALHOST_PORT = pkg.gulp.developmentPort;
 export const BROWSER_SYNC_PORT = pkg.gulp.browserSyncPort;
 
 export const FILES_TO_INJECT_FOR_DIST = [
