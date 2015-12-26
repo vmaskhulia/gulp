f3d1351d0d66ae97ff7813629dbb009795889a62
Vato Maskhulia <vato.maskhulia@gmail.com>
2015-12-26 08:00:52 +0400

    update

--- tasks/release.js
+++ tasks/release.js
@@ -17,7 +17,7 @@ gulp.task('release', ['dist'], () => {
 
   var proc = exec(`
       ssh ${address} "mkdir -p ${NAME} && rm -r -f ${NAME}/dist ${NAME}/server"
-      scp -r ${rootPath}/dist ${rootPath}/server ${rootPath}/package.json ${address}:~/${NAME}
+      rsync -a ${rootPath}/dist ${rootPath}/server ${rootPath}/package.json ${address}:~/${NAME}
       ssh ${address} ". ~/.nvm/nvm.sh && cd ${NAME} && npm i --production --no-optional"
     `);
 
