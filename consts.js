f84c0066fe8eaf7642437ba2033382c12fe86d0b
Vato Maskhulia <vato.maskhulia@gmail.com>
2016-02-04 20:31:08 +0400

    chore(update)

--- consts.js
+++ consts.js
@@ -3,8 +3,8 @@
 import pkg from '../package.json';
 
 export const NAME = pkg.name;
-export const SSH_ADDRESS = pkg.gulp.sshAddress;
-export const LOCALHOST_PORT = pkg.gulp.developmentPort;
-export const BROWSER_SYNC_PORT = pkg.gulp.browserSyncPort;
+export const SSH_ADDRESS = pkg.config.sshAddress;
+export const LOCALHOST_PORT = pkg.config.developmentPort;
+export const BROWSER_SYNC_PORT = pkg.config.browserSyncPort;
 export const LUCY_SSH_ADDRESS = 'lucy@46.101.135.12';
-export const JSPM_PACKAGES_FOR_DIST = pkg.gulp.fontsPaths;
+export const JSPM_PACKAGES_FOR_DIST = pkg.config.fontsPaths;
