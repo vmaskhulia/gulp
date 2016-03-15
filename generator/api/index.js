54a21aed6de3b2c1cf8d7cc2bb8ba37db4f60822
Vato Maskhulia <vato.maskhulia@gmail.com>
2016-03-15 11:47:20 +0400

    chore(update)

--- generator/api/index.js
+++ generator/api/index.js
@@ -10,54 +10,50 @@ var auth = require('../../auth');
 module.exports = router;
 
 
-router.get('/', parser.parseGetByQuery, co.wrap(getByQuery));
+router.get('/', parser.parseGetByQuery, getByQuery);
 
-router.post('/', auth.isAdmin(), parser.parseCreate, co.wrap(create));
-router.post('/update', auth.isAdmin(), parser.parseUpdate, co.wrap(update));
+router.post('/', auth.isAdmin(), parser.parseCreate, create);
+router.post('/update', auth.isAdmin(), parser.parseUpdate, update);
 
-router.delete('/:<%=nameLC%>Id', auth.isAdmin(), parser.parseDestroy, co.wrap(destroy));
+router.delete('/:<%=nameLC%>Id', auth.isAdmin(), parser.parseDestroy, destroy);
 
 
 // =============== GET ===============
 
-function* getByQuery(req, res, next) {
-  try {
+function getByQuery(req, res, next) {
+  co(function* () {
     var q = req.parsed;
     var <%=namePlural%>Data = yield <%=nameUC%>.getByQuery(q.findQuery, q.orQuery, q.sortBy, q.offset, q.limit);
     res.json(<%=namePlural%>Data);
-  } catch (e) {
-    next(e);
-  }
+  })
+  .catch(next);
 }
 
 // =============== POST ===============
 
-function* create(req, res, next) {
-  try {
+function create(req, res, next) {
+  co(function* () {
     var parsed<%=nameUC%> = req.parsed;
     yield <%=nameUC%>.create(parsed<%=nameUC%>);
     res.sendStatus(201);
-  } catch (e) {
-    next(e);
-  }
+  })
+  .catch(next);
 }
 
-function* update(req, res, next) {
-  try {
+function update(req, res, next) {
+  co(function* () {
     var parsed<%=nameUC%> = req.parsed;
     yield <%=nameUC%>.update(parsed<%=nameUC%>._id, parsed<%=nameUC%>);
     res.sendStatus(200);
-  } catch (e) {
-    next(e);
-  }
+  })
+  .catch(next);
 }
 
-function* destroy(req, res, next) {
-  try {
+function destroy(req, res, next) {
+  co(function* () {
     var <%=nameLC%>Id = req.parsed.<%=nameLC%>Id;
     yield <%=nameUC%>.destroy(<%=nameLC%>Id);
     res.sendStatus(200);
-  } catch (e) {
-    next(e);
-  }
+  })
+  .catch(next);
 }
