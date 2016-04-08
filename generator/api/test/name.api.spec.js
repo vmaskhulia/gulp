a50a05d8c47fa8ab146ecd1ee8064899157b38dd
Vato Maskhulia <vato.maskhulia@gmail.com>
2016-04-08 18:28:27 +0400

    chore(update)

---
+++ generator/api/test/name.api.spec.js
@@ -0,0 +1,65 @@
+'use strict';
+
+var co = require('co');
+var testHelpers = require('../../../helpers/testHelpers');
+var agents = require('../../users/test/agents');
+var CarStub = require('../../../stubs/<%=nameLC%>.stub');
+
+
+describe('api/<%=namePlural%>', () => {
+  let guestAgent = agents.getGuestAgent();
+  let clientAgent = agents.getClientAgent();
+  let adminAgent = agents.getAdminAgent();
+
+  before(co.wrap(function* () {
+    testHelpers.connectDB();
+    yield testHelpers.clearDB();
+  }));
+
+  before((done) => {
+    clientAgent.authorize(() => {
+      adminAgent.authorize(done);
+    });
+  });
+
+  after(testHelpers.clearDB);
+
+  // =============== getters ===============
+
+  describe('GET /', () => {
+    let route = '/api/<%=namePlural%>';
+
+    it('should return 200 for guest', (done) => {
+      guestAgent.get(route).expect(200, done);
+    });
+
+    it('should return 200 for client', (done) => {
+      clientAgent.get(route).expect(200, done);
+    });
+
+    it('should return 200 for admin', (done) => {
+      adminAgent.get(route).expect(200, done);
+    });
+  });
+
+  describe('POST /', () => {
+    let route = '/api/<%=namePlural%>';
+    let <%=nameLC%>Stub;
+
+    beforeEach(() => {
+      <%=nameLC%>Stub = CarStub.getSingle();
+    });
+
+    it('should return 401 for guest', (done) => {
+      guestAgent.post(route).send(<%=nameLC%>Stub).expect(401, done);
+    });
+
+    it('should return 401 for client', (done) => {
+      clientAgent.post(route).send(<%=nameLC%>Stub).expect(401, done);
+    });
+
+    it('should return 201 for admin', (done) => {
+      adminAgent.post(route).send(<%=nameLC%>Stub).expect(201, done);
+    });
+  });
+});
