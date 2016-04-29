ed947ff467fdd3431809cb0e71b6cb036b90aba4
Vato Maskhulia <vato.maskhulia@gmail.com>
2016-04-29 20:34:16 +0400

    chore(update)

--- consts.js
+++ consts.js
@@ -2,7 +2,7 @@
 
 import pkg from '../package.json';
 
-export const LUCY_SSH_ADDRESS = 'lucy@46.101.135.12';
+export const LUCY_SSH_ADDRESS = pkg.config.lucySSHAddress;
 export const NAME = pkg.name;
 export const SSH_ADDRESS = pkg.config.sshAddress;
 export const DEVELOPMENT_PORT = pkg.config.developmentPort;
