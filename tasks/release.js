39ebb2ca5c7a47c81038690f7ff9b57895a002bc
Vato Maskhulia <vato.maskhulia@gmail.com>
2016-08-08 12:07:00 +0400

    chore(update)

--- tasks/release.js
+++ tasks/release.js
@@ -3,22 +3,22 @@
 import gulp from 'gulp';
 import {NAME, SSH_ADDRESS, LUCY_SSH_ADDRESS} from '../consts';
 import paths from '../paths';
-var $ = require('gulp-load-plugins')();
+const exec = require('child_process').exec;
+const $ = require('gulp-load-plugins')();
 
 
 gulp.task('release', ['dist'], () => {
-  var argv = $.util.env;
-  var useLucyServer = argv.lucy || argv.l;
-  var startApp = argv.start || argv.s;
-  var address = useLucyServer ? LUCY_SSH_ADDRESS : SSH_ADDRESS;
-  var startScript = startApp ? 'export NODE_ENV=production && forever start server/server.js' : 'forever restartall';
-  var exec = require('child_process').exec;
+  const argv = $.util.env;
+  const useLucyServer = argv.lucy || argv.l;
+  const startApp = argv.start || argv.s;
+  const address = useLucyServer ? LUCY_SSH_ADDRESS : SSH_ADDRESS;
+  const startScript = startApp ? 'export NODE_ENV=production && forever start server/server.js' : 'forever restartall';
 
-  var proc = exec(`
+  const proc = exec(`
       cd ${paths.root}
       ssh ${address} "mkdir -p ${NAME} && cd ${NAME} && rm -rf dist server"
       rsync -a dist server package.json ${address}:~/${NAME}
-      ssh ${address} ". ~/.nvm/nvm.sh && cd ${NAME} && npm i --production --no-optional && ${startScript}"
+      ssh ${address} ". .nvm/nvm.sh && cd ${NAME} && npm i --production --no-optional && ${startScript}"
     `);
 
   proc.stdout.on('data', data => console.info(data));
